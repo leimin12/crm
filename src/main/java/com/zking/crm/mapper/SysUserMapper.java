f656c28794672aa4b019e85fc615174e212a8434
leimin12 <1360545821@qq.com>
2018-03-11 14:35:32 +0800

    保存到本地并且推送到代码管理网站

---
+++ src/main/java/com/zking/crm/mapper/SysUserMapper.java
@@ -0,0 +1,19 @@
+package com.zking.crm.mapper;
+
+import com.zking.crm.model.SysUser;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SysUserMapper {
+    int deleteByPrimaryKey(Integer userId);
+
+    int insert(SysUser record);
+
+    int insertSelective(SysUser record);
+
+    SysUser selectByPrimaryKey(Integer userId);
+
+    int updateByPrimaryKeySelective(SysUser record);
+
+    int updateByPrimaryKey(SysUser record);
+}
