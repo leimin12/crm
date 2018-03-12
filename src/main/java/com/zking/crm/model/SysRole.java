97d60e79d9c069e5137e71ef477c5d9af75cee00
leimin12 <1360545821@qq.com>
2018-03-12 15:59:55 +0800

    保存到本地并且推送到代码管理网站

---
+++ src/main/java/com/zking/crm/model/SysRole.java
@@ -0,0 +1,54 @@
+package com.zking.crm.model;
+
+public class SysRole {
+    private Integer roleId;
+
+    private String roleName;
+
+    private String roleDesc;
+
+    private Integer roleFlag;
+
+    public SysRole(Integer roleId, String roleName, String roleDesc, Integer roleFlag) {
+        this.roleId = roleId;
+        this.roleName = roleName;
+        this.roleDesc = roleDesc;
+        this.roleFlag = roleFlag;
+    }
+
+    public SysRole() {
+        super();
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleDesc() {
+        return roleDesc;
+    }
+
+    public void setRoleDesc(String roleDesc) {
+        this.roleDesc = roleDesc;
+    }
+
+    public Integer getRoleFlag() {
+        return roleFlag;
+    }
+
+    public void setRoleFlag(Integer roleFlag) {
+        this.roleFlag = roleFlag;
+    }
+}
