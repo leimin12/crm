dfb9aaae52429f2b401f473f4068dbaafb67aaf2
leimin12 <1360545821@qq.com>
2018-03-14 21:28:05 +0800

    保存到本地并且推送到代码管理网站

---
+++ src/main/java/com/zking/crm/controller/LostController.java
@@ -0,0 +1,36 @@
+package com.zking.crm.controller;
+
+import com.zking.crm.biz.ILostBiz;
+import com.zking.crm.model.Lost;
+import com.zking.crm.util.PageBean;
+import com.zking.crm.util.ResponseData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+@RequestMapping("lostCont")
+public class LostController {
+
+    @Autowired
+    private ILostBiz lostBiz;
+
+    @RequestMapping("doToLost")
+    public String doToLost() {
+        return "rept/lost";
+    }
+
+    @RequestMapping("listLost")
+    @ResponseBody
+    public ResponseData listLost(Lost lost, HttpServletRequest request, PageBean pageBean, ResponseData responseData) {
+        pageBean.setRequest(request);
+        List<Lost> losts = lostBiz.listLost(lost, pageBean);
+        responseData.setTotal(pageBean.getTotalRecord());// 总行数
+        responseData.setRows(losts);// 类似于// List/Set/Array的集合
+        return responseData;
+    }
+}
---
+++ src/main/java/com/zking/crm/controller/SatisfyController.java
@@ -0,0 +1,31 @@
+package com.zking.crm.controller;
+
+import com.zking.crm.biz.ISatisfyBiz;
+import com.zking.crm.model.Satisfy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("Satisfy")
+public class SatisfyController {
+
+    @Autowired
+    private ISatisfyBiz satisfyBiz;
+
+    @RequestMapping("doToSer")
+    public String doToSer() {
+        return "rept/ser";
+    }
+
+
+    @RequestMapping("doGetCount2")
+    @ResponseBody
+    public List<Integer> doGetCount2(Satisfy satisfy) {
+        return satisfyBiz.doGetCount2(satisfy);
+    }
+
+}
